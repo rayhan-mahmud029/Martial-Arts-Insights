51ede4018b238479cb3c72614abccd0d604ce314
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-06-15 10:04:35 +0600

    folder structure set and routes, google fonts added

---
+++ src/Shared/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='sticky space-y-4'>
+            <div className="w-11/12 mx-auto">
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default NavBar;
