9ebd2e35ff5d61d920c04676c62a9c9a7da838b4
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-06-16 15:18:19 +0600

    popular instructors section added and data displayed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,11 @@ import { useQuery } from '@tanstack/react-query';
 import HeadingCover from '../components/HeadingCover';
 import axios from 'axios';
 
+
 const Home = () => {
+    const [open, setOpen] = React.useState(true);
+
+    // query for classes data
     const { data: classes = [], error } = useQuery(['classes'], async () => {
         try {
             const response = await axios.get('http://localhost:5000/classes', {
@@ -22,22 +26,40 @@ const Home = () => {
         }
     });
 
-    console.log(classes);
+    // query for instructors data
+    const { data: instructors = [], error: errorInInstructors } = useQuery(['instructors'], async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/instructors', {
+                params: {
+                    sortField: 'numOfStudents',
+                    sortOrder: 'dsc',
+                },
+            });
+            if (response.status !== 200) {
+                throw new Error('Network response was not ok');
+            }
+            return response.data;
+        } catch (errorInInstructors) {
+            throw new Error('Error fetching data');
+        }
+    });
+
+    console.log(classes, instructors);
 
     return (
         <div className='w-11/12 mx-auto'>
             <TopSlider />
 
-             
+
 
             {/* POPULAR CLASSES SECTION */}
             <section className="my-6">
                 <HeadingCover img={'https://i.ibb.co/H7MCkMV/preschooler-boy-dressed-white-karate-kimono-with-orange-belt-min.jpg'} title={'Popular Classes'} description={'Learn the fundamental techniques and principles'}></HeadingCover>
 
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 my-6'>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 my-12'>
                     {
-                        classes.slice(0, 6).map(classItem => <div key={classItem._id} className="card w-96 bg-base-100 shadow-xl image-full">
-                            <figure><img src={classItem.image} alt="Shoes" /></figure>
+                        classes.slice(0, 6).map(classItem => <div key={classItem._id} className="card w-96 bg-base-100 shadow-xl image-full hover:scale-110 hover:-translate-x-5 hover:-translate-y-5 hover:z-50 transition">
+                            <figure><img src={classItem.image} alt="Shoes" className='' /></figure>
                             <div className="card-body mt-16">
                                 <h2 className="card-title font-poppins">{classItem.name}</h2>
                                 <p className='text-xs font-sans'>Enrolled Students: {classItem.enrolledStudents}</p>
@@ -53,7 +75,23 @@ const Home = () => {
 
 
             <section className="my-6">
-            <HeadingCover img={'https://i.ibb.co/PcgD13S/brutal-angry-man-white-shirt-is-wearing-protection-his-fist-before-fight-dark-studio-min.jpg'} title={'Popular Instructors'} description={'Experienced martial art instructors offering a variety of classes, fostering skill development and personal growth in a supportive learning environment.'}></HeadingCover>
+                <HeadingCover img={'https://i.ibb.co/PcgD13S/brutal-angry-man-white-shirt-is-wearing-protection-his-fist-before-fight-dark-studio-min.jpg'} title={'Popular Instructors'} description={'Experienced martial art instructors offering a variety of classes, fostering skill development and personal growth in a supportive learning environment.'}></HeadingCover>
+
+                <div className="grid grid-cols-2 lg:grid-cols-3 gap-8 my-14">
+                    {instructors.slice(0, 6).map((instructor) => (
+                        <div className="card card-compact bg-base-100 shadow-xl transform skew-y-6">
+                            <figure >
+                                <img src={instructor.image} alt="Shoes" className="w-full h-[350px] object-cover" style={{ maxHeight: "100%", maxWidth: "100%" }} />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title font-poppins">{instructor.name}</h2>
+                            </div>
+                        </div>
+
+                    ))}
+                </div>
+
+
 
             </section>
 
